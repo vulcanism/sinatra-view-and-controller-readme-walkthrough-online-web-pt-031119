b5b0835fc605a45faba46c0b9ed9b7641e403e21
Savannah C <sgcorrell@gmail.com>
2019-06-13 16:17:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,13 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/reverse' do
-    original_string = params["string"]
-    @reversed_string = original_string.reverse
-    
     erb :reverse
   end
 
   post '/reverse' do
+    original_string = params["string"]
+    @reversed_string = original_string.reverse
+    
     erb :reversed
   end
 
